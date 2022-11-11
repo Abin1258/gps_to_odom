8ac5926c6a00ad505385e736ce42482a9a3156c1
Abin1258 <jiashangbin>
2022-11-11 10:33:30 +0800

    master

--- include/gps_to_odom/gps_to_odom.hpp
+++ include/gps_to_odom/gps_to_odom.hpp
@@ -44,7 +44,7 @@ public:
 
 GpsOdom::GpsOdom(ros::NodeHandle& nh):nhg(nh) {
   //参数初始化
-  navsatsub = nhg.subscribe("/gps/fix", 32, &GpsOdom::NavsatCallback, this);
+  navsatsub = nhg.subscribe("/gps/fix", 32, &GpsOdom::NavsatCallback, this); //gps/fix
   odompub = nhg.advertise<nav_msgs::Odometry>("gps_odom", 32);
   
   odom.header.frame_id = "odom";
@@ -73,12 +73,22 @@ void GpsOdom::GpsCallback(const geographic_msgs::GeoPointStampedPtr& gps_msg) {
     gpspoint.altitude = gps_msg->position.altitude;
     gpspoint.latitude = gps_msg->position.latitude;
     gpspoint.longitude = gps_msg->position.longitude;
-    geodesy::UTMPoint utm;
-    geodesy::fromMsg(gpspoint, utm);
-    Eigen::Vector3d xyz(utm.easting, utm.northing, utm.altitude);
+    geodesy::UTMPoint utm;//创建一个utm坐标
+    geodesy::fromMsg(gpspoint, utm);//把一帧gps消息转化为utm格式
+    Eigen::Vector3d xyz(utm.easting, utm.northing, utm.altitude);//创建一个xyz坐标向量
 
-      if(!zero_utm) {
-        zero_utm = xyz;
+    //有局部坐标经纬度的时候启用
+    // geographic_msgs::GeoPoint gpspoint0;
+    // gpspoint0.altitude = 局部原点高度;
+    // gpspoint0.latitude = xxx;
+    // gpspoint0.longitude = xxx;
+    // geodesy::UTMPoint utm0;
+    // geodesy::fromMsg(gpspoint0, utm0);
+    // Eigen::Vector3d xyz0(utm0.easting, utm0.northing, utm0.altitude);
+
+      if(!zero_utm) {//如果第一帧
+        zero_utm = xyz;//把第一帧设置为原点
+        //zero_utm = xyz0；//有局部坐标经纬度的时候启用
       }
       xyz -= (*zero_utm);
 
